dd3a953f2b331cb84bf40b15d82f7d5e06d7788c
Gonzalo Bulnes Guilpain <gon.bulnes@fastmail.com>
2020-07-31 23:38:51 +1000

    Refactor remove obsolete code

--- cmd/main.go
+++ cmd/main.go
@@ -7,40 +7,9 @@ import (
 	"time"
 
 	"github.com/gonzalo-bulnes/scanner"
-	"github.com/gonzalo-bulnes/scanner/cmd/securedrop"
+	"github.com/gonzalo-bulnes/scanner/cmd/securedrop/instance"
 	"github.com/gonzalo-bulnes/scanner/cmd/tor"
 )
-
-// example service which status check takes time.
-type example struct {
-	duration time.Duration
-	status   string
-	name     string
-}
-
-// Check is an example check function that supports cancellation.
-func (e example) Check(ctx context.Context) scanner.Status {
-	select {
-	case <-ctx.Done():
-		return exampleStatus{err: fmt.Errorf("%s: %w", e.name, ctx.Err())}
-	case <-time.After(e.duration):
-		return exampleStatus{value: e.status}
-	}
-}
-
-// exampleStatus also conveys errors.
-type exampleStatus struct {
-	value string
-	err   error
-}
-
-func (s exampleStatus) Value() interface{} {
-	return s.value
-}
-
-func (s exampleStatus) Err() error {
-	return s.err
-}
 
 func main() {
 	client, err := tor.NewClient()
@@ -50,7 +19,7 @@ func main() {
 	}
 
 	services := []scanner.Service{
-		securedrop.NewInstance(client, "missing.onion"),
+		instance.New(client, "missing.onion"),
 	}
 
 	s := scanner.New()
@@ -59,15 +28,13 @@ func main() {
 	defer cancel()
 
 	output := make(chan scanner.Status, len(services))
-	done := make(chan bool, 1)
-	go s.Scan(ctx, output, done, services...)
+	go s.Scan(ctx, output, services...)
 
 	for status := range output {
 		if err := status.Err(); err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println(status.Value().(*securedrop.Instance).CSV())
+			fmt.Println(status.Value().(instance.Metadata))
 		}
 	}
-	<-done
 }
